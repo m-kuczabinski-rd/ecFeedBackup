b6b2dc4135562d2dd769c776c8d027d59d90b677
Patryk Chamuczynski <p.chamuczynski@radytek.com>
2013-05-29 16:05:25 +0200

    name change cosmetics

---
+++ ecfeed/src/com/testify/ecfeed/editors/XmlColorConstants.java
@@ -0,0 +1,16 @@
+package com.testify.ecfeed.editors;
+
+import org.eclipse.swt.graphics.RGB;
+
+public interface XmlColorConstants {
+	RGB XML_COMMENT = new RGB(128, 0, 0);
+	RGB PROC_INSTR = new RGB(128, 128, 128);
+	RGB STRING = new RGB(0, 128, 0);
+	RGB DEFAULT = new RGB(0, 0, 0);
+	RGB TAG = new RGB(0, 0, 128);
+
+	//enhancements
+	RGB ESCAPED_CHAR = new RGB(128, 128, 0);
+	RGB CDATA = new RGB(0, 128, 128);
+	RGB CDATA_TEXT = new RGB(255, 0, 0);
+}
