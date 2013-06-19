7bdceef3395aef0baeb78a1a480c648e0f4afcb1
Patryk Chamuczynski <p.chamuczynski@radytek.com>
2013-06-19 11:51:56 +0200

    test for EcFeed runner

---
+++ ecfeed/test/com/testify/ecfeed/runner/EcFeederTest.java
@@ -0,0 +1,30 @@
+package com.testify.ecfeed.runner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.testify.ecfeed.runner.annotations.EcModel;
+import com.testify.ecfeed.runner.annotations.TestSuites;
+
+@RunWith(EcFeeder.class)
+public class EcFeederTest{
+
+	public EcFeederTest() throws Throwable {
+	}
+	
+	@EcModel
+	public static String[] getModel(){
+		return new String[]{"test/com/testify/ecfeed/runner/ecModel.ect"};
+	}
+	
+	@TestSuites
+	public static String[] testSuites(){
+		return new String[]{"dupa", "default suite"};
+	}
+	
+	@Test
+	public void exampleTestFunction(int intParameter, String stringParameter){
+		System.out.println("exampleTestFunction(" + intParameter + ", " + stringParameter + ")");
+	}
+
+}
