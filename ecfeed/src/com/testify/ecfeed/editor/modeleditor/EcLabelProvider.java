1fee5ab61988a2e5a881ab127ed6835f52864db4
p.chamuczynski@radytek.com <p.chamuczynski@radytek.com>
2013-08-01 08:39:06 +0200

    introduced icons for ec tree

--- ecfeed/src/com/testify/ecfeed/editor/modeleditor/EcLabelProvider.java
+++ ecfeed/src/com/testify/ecfeed/editor/modeleditor/EcLabelProvider.java
@@ -10,7 +10,13 @@ import org.eclipse.swt.graphics.Image;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.FrameworkUtil;
 
+import com.testify.ecfeed.model.CategoryNode;
+import com.testify.ecfeed.model.ClassNode;
 import com.testify.ecfeed.model.GenericNode;
+import com.testify.ecfeed.model.MethodNode;
+import com.testify.ecfeed.model.PartitionNode;
+import com.testify.ecfeed.model.RootNode;
+import com.testify.ecfeed.model.TestCaseNode;
 
 public class EcLabelProvider extends LabelProvider {
 
@@ -22,6 +28,24 @@ public class EcLabelProvider extends LabelProvider {
 	}
 	
 	public Image getImage(Object element){
+		if(element instanceof RootNode){
+			return getImage("root_node.gif");
+		}
+		if(element instanceof ClassNode){
+			return getImage("class_node.gif");
+		}
+		if(element instanceof MethodNode){
+			return getImage("method_node.gif");
+		}
+		if(element instanceof TestCaseNode){
+			return getImage("test_case_node.gif");
+		}
+		if(element instanceof CategoryNode){
+			return getImage("category_node.gif");
+		}
+		if(element instanceof PartitionNode){
+			return getImage("partition_node.gif");
+		}
 		return getImage("sample.gif");
 	}
 	
