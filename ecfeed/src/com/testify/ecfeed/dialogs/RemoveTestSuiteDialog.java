d9b3a04b5dc9c336d8a5a69b80bd05a982bb4642
p.chamuczynski@radytek.com <p.chamuczynski@radytek.com>
2013-08-01 10:55:41 +0200

    Applied copyrights

--- ecfeed/src/com/testify/ecfeed/dialogs/RemoveTestSuiteDialog.java
+++ ecfeed/src/com/testify/ecfeed/dialogs/RemoveTestSuiteDialog.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Testify AS.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Patryk Chamuczynski (p.chamuczynski(at)gmail.com) - initial implementation
+ ******************************************************************************/
+
 package com.testify.ecfeed.dialogs;
 
 import java.util.Set;
