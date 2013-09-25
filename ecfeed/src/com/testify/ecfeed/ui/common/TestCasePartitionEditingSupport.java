5c33f22632c91076b06ab129a6a264ce45713d9c
p.chamuczynski@radytek.com <p.chamuczynski@radytek.com>
2013-09-25 15:58:24 +0200

    Unified Patrition editing support with value editing support

--- ecfeed/src/com/testify/ecfeed/ui/common/TestCasePartitionEditingSupport.java
+++ ecfeed/src/com/testify/ecfeed/ui/common/TestCasePartitionEditingSupport.java
@@ -19,17 +19,18 @@ import org.eclipse.jface.viewers.ComboBoxViewerCellEditor;
 import org.eclipse.jface.viewers.EditingSupport;
 import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.TextCellEditor;
 import org.eclipse.swt.SWT;
 
 import com.testify.ecfeed.model.CategoryNode;
-import com.testify.ecfeed.model.ExpectedValueCategoryNode;
-import com.testify.ecfeed.model.MethodNode;
 import com.testify.ecfeed.model.PartitionNode;
+import com.testify.ecfeed.utils.EcModelUtils;
 
 public class TestCasePartitionEditingSupport extends EditingSupport {
 	private final TableViewer fViewer;
 	private ArrayList<PartitionNode> fTestData;
-	private ComboBoxViewerCellEditor fCellEditor;
+	private ComboBoxViewerCellEditor fComboCellEditor = null;
+	private TextCellEditor fTextCellEditor;
 	private IInputChangedListener fSetValueListener;
 
 	public TestCasePartitionEditingSupport(TableViewer viewer, ArrayList<PartitionNode> testData, IInputChangedListener setValueListener) {
@@ -37,46 +38,73 @@ public class TestCasePartitionEditingSupport extends EditingSupport {
 		fViewer = viewer;
 		fTestData = testData;
 		fSetValueListener = setValueListener;
-		fCellEditor = new ComboBoxViewerCellEditor(fViewer.getTable(), SWT.TRAIL);
-		fCellEditor.setLabelProvider(new LabelProvider());
-		fCellEditor.setContentProvider(new ArrayContentProvider());
-		fCellEditor.setActivationStyle(ComboBoxViewerCellEditor.DROP_DOWN_ON_KEY_ACTIVATION | 
-				ComboBoxViewerCellEditor.DROP_DOWN_ON_MOUSE_ACTIVATION);
 	}
 
 	@Override
 	protected CellEditor getCellEditor(Object element) {
 		PartitionNode partition = (PartitionNode)element;
-		CategoryNode parent = (CategoryNode)partition.getParent();
-		fCellEditor.setInput(parent.getPartitions());
-		fCellEditor.setValue(partition.getName());
-		return fCellEditor;
+		if(partition.getCategory().isExpected()){
+			return getTextCellEditor(partition);
+		}
+		else{
+			return getComboCellEditor(partition);
+		}
+	}
+
+	private CellEditor getComboCellEditor(PartitionNode partition) {
+		if(fComboCellEditor == null){
+			fComboCellEditor = new ComboBoxViewerCellEditor(fViewer.getTable(), SWT.TRAIL);
+			fComboCellEditor.setLabelProvider(new LabelProvider());
+			fComboCellEditor.setContentProvider(new ArrayContentProvider());
+			fComboCellEditor.setActivationStyle(ComboBoxViewerCellEditor.DROP_DOWN_ON_KEY_ACTIVATION | 
+					ComboBoxViewerCellEditor.DROP_DOWN_ON_MOUSE_ACTIVATION);
+		}
+		fComboCellEditor.setInput(partition.getCategory().getPartitions());
+		fComboCellEditor.setValue(partition);
+		return fComboCellEditor;
+	}
+
+	private CellEditor getTextCellEditor(PartitionNode partition) {
+		if(fTextCellEditor == null){
+			fTextCellEditor = new TextCellEditor(fViewer.getTable(), SWT.LEFT); 
+		}
+		String valueString = partition.getValueString();
+		fTextCellEditor.setValue(valueString);
+		return fTextCellEditor;
 	}
 
 	@Override
 	protected boolean canEdit(Object element) {
-		if(element instanceof PartitionNode){
-			PartitionNode partition = (PartitionNode) element;
-			CategoryNode parent = partition.getCategory();
-			boolean canEdit = !(parent instanceof ExpectedValueCategoryNode); 
-			return canEdit;
-		}
-		return false;
+		return true;
 	}
 
 	@Override
 	protected Object getValue(Object element) {
-		return ((PartitionNode)element).toString();
+		PartitionNode partition = (PartitionNode)element;
+		if(partition.getCategory().isExpected()){
+			return partition.getValueString();
+		}
+		return partition.toString();
 	}
 
 	@Override
 	protected void setValue(Object element, Object value) {
 		CategoryNode parent = (CategoryNode)((PartitionNode)element).getParent();
-		MethodNode method = (MethodNode)parent.getParent();
-		int parentIndex = method.getCategories().indexOf(parent);
-		if(parentIndex >= 0 && parentIndex <= fTestData.size()){
-			fTestData.set(parentIndex, (PartitionNode)value);
+		if(value instanceof String && parent.isExpected()){
+			String valueString = (String)value;
+			if(EcModelUtils.validatePartitionStringValue(valueString, parent)){
+				Object newValue = EcModelUtils.getPartitionValueFromString(valueString, parent.getType());
+				((PartitionNode)element).setValue(newValue);
+			}
 		}
+		else if(value instanceof PartitionNode){
+			PartitionNode partition = (PartitionNode)value;
+			int parentIndex = parent.getMethod().getCategories().indexOf(parent);
+			if(parentIndex >= 0 && parentIndex <= fTestData.size()){
+				fTestData.set(parentIndex, partition);
+			}
+		}
+		
 		fSetValueListener.inputChanged();
 	}
 }
