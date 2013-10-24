90f40fccee1a9775e77b3e6a2882274e0c868030
p.chamuczynski@radytek.com <p.chamuczynski@radytek.com>
2013-10-24 16:22:17 +0200

    new API for generator supporting progressive data generation;

--- ecfeed/src/com/testify/ecfeed/api/IGenerator.java
+++ ecfeed/src/com/testify/ecfeed/api/IGenerator.java
@@ -11,10 +11,40 @@
 
 package com.testify.ecfeed.api;
 
+import java.util.Collection;
 import java.util.List;
 
+import org.eclipse.core.runtime.IProgressMonitor;
+
 public interface IGenerator<E> {
-	public void initialize(List<List<E>> inputDomain);
-	public String[] getAlgorithms();
-	public IAlgorithm<E> getAlgorithm(String name);
+	/*
+	 * returns list of parameters used by this generator.
+	 */
+	
+	public List<IGeneratorParameter> requiredParameters();
+	/*
+	 * returns list of parameters used by this generator. The returned parameter list should not 
+	 * differ from the same function without parameters, but might be more detailed regarding 
+	 * allowed set of values for parameter. 
+	 */
+	public List<IGeneratorParameter> requiredParameters(List<List<E>> inputDomain);
+	
+	/*
+	 * Sets value of generator's parameter.
+	 */
+	public void setParameter(String name, Object value) throws GeneratorException;
+
+	/*
+	 * Should be called prior to first call of getNext()
+	 */
+	public void initialize(List<List<E>> inputDomain, 
+			Collection<IConstraint<E>> constraints,
+			IProgressMonitor progressMonitor) throws GeneratorException;
+	
+	/*
+	 * Returns null if no more data can be generated, e.g. due to lack of initialization or 
+	 *  if the test generation should end according to the used algorithm or provided
+	 *  parameter. Blocking method, implementation should make be cancelable using progressMonitor.
+	 */
+	public List<E> getNext() throws GeneratorException;
 }
