af1546d9bd9c568573775053d190d9591e6e3183
p.chamuczynski@radytek.com <p.chamuczynski@radytek.com>
2013-11-09 15:41:18 +0100

    rawtype fix in Generator annotation

--- ecfeed/src/com/testify/ecfeed/runner/annotations/Generator.java
+++ ecfeed/src/com/testify/ecfeed/runner/annotations/Generator.java
@@ -6,10 +6,10 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 import com.testify.ecfeed.api.IGenerator;
-import com.testify.ecfeed.model.PartitionNode;
 
 @Target({ElementType.METHOD, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Generator {
-	Class<IGenerator<PartitionNode>> value();
+	@SuppressWarnings("rawtypes")
+	Class<? extends IGenerator> value();
 }
