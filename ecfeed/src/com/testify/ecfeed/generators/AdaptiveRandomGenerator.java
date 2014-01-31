efeb717deb00d94509c3915ef83b5f31f1571381
p.chamuczynski@radytek.com <p.chamuczynski@radytek.com>
2014-01-31 11:58:03 +0100

    renamed AdaptiveRandomGenerator parameters

--- ecfeed/src/com/testify/ecfeed/generators/AdaptiveRandomGenerator.java
+++ ecfeed/src/com/testify/ecfeed/generators/AdaptiveRandomGenerator.java
@@ -9,18 +9,18 @@ import com.testify.ecfeed.generators.api.GeneratorException;
 import com.testify.ecfeed.generators.api.IConstraint;
 
 public class AdaptiveRandomGenerator<E> extends AbstractGenerator<E> {
-	public final String EXECUTED_SET_SIZE_PARAMETER_NAME = "Executed set size";
-	public final int DEFAULT_EXECUTED_SET_SIZE_VALUE = -1;
+	public final String HISTORY_DEPTH_PARAMETER_NAME = "Depth";
+	public final int DEFAULT_HISTORY_DEPTH_VALUE = -1;
 	public final String CANDIDATE_SET_SIZE_PARAMETER_NAME = "Candidate set size";
 	public final int DEFAULT_CANDIDATE_SET_SIZE_PARAMETER_VALUE = 100;
-	public final String TEST_SUITE_SIZE_PARAMETER_NAME = "Test suite size";
+	public final String TEST_SUITE_SIZE_PARAMETER_NAME = "Length";
 	public final int DEFAULT_TEST_SUITE_SIZE_PARAMETER_VALUE = 1;
 	public final String DUPLICATES_PARAMETER_NAME = "Duplicates";
 	public final boolean DEFAULT_DUPLICATES_PARAMETER_VALUE = false;
 
 	public AdaptiveRandomGenerator() throws GeneratorException{
-		addParameterDefinition(new IntegerParameter(EXECUTED_SET_SIZE_PARAMETER_NAME, 
-				false, DEFAULT_EXECUTED_SET_SIZE_VALUE, -1, Integer.MAX_VALUE));
+		addParameterDefinition(new IntegerParameter(HISTORY_DEPTH_PARAMETER_NAME, 
+				false, DEFAULT_HISTORY_DEPTH_VALUE, -1, Integer.MAX_VALUE));
 		addParameterDefinition(new IntegerParameter(CANDIDATE_SET_SIZE_PARAMETER_NAME, 
 				true, DEFAULT_CANDIDATE_SET_SIZE_PARAMETER_VALUE, 0, Integer.MAX_VALUE));
 		addParameterDefinition(new IntegerParameter(TEST_SUITE_SIZE_PARAMETER_NAME, 
@@ -35,7 +35,7 @@ public class AdaptiveRandomGenerator<E> extends AbstractGenerator<E> {
 			Map<String, Object> parameters) throws GeneratorException{
 		
 		super.initialize(inputDomain, constraints, parameters);
-		int executedSetSize = getIntParameter(EXECUTED_SET_SIZE_PARAMETER_NAME);
+		int executedSetSize = getIntParameter(HISTORY_DEPTH_PARAMETER_NAME);
 		int candidateSetSize = getIntParameter(CANDIDATE_SET_SIZE_PARAMETER_NAME);
 		int testSuiteSize = getIntParameter(TEST_SUITE_SIZE_PARAMETER_NAME);
 		boolean duplicates = getBooleanParameter(DUPLICATES_PARAMETER_NAME);
