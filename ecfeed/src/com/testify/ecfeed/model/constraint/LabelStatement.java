874b358c50527b7135d0bdf5ccf93a82cb9f37c7
p.chamuczynski@radytek.com <p.chamuczynski@radytek.com>
2014-02-06 08:40:59 +0100

    implementation of LabelStatement

---
+++ ecfeed/src/com/testify/ecfeed/model/constraint/LabelStatement.java
@@ -0,0 +1,62 @@
+package com.testify.ecfeed.model.constraint;
+
+import java.util.List;
+
+import com.testify.ecfeed.model.CategoryNode;
+import com.testify.ecfeed.model.PartitionNode;
+
+public class LabelStatement extends BasicStatement {
+	private Relation fRelation;
+	private String fConditionLabel;
+	private CategoryNode fCategory;
+
+	public LabelStatement(CategoryNode category, String conditionLabel, Relation relation){
+		fCategory = category;
+		fConditionLabel = conditionLabel;
+		fRelation = relation;
+	}
+	
+	public String getCondition(){
+		return fConditionLabel;
+	}
+	
+	public void setCondition(String condition) {
+		fConditionLabel = condition;
+	}
+
+	public Relation getRelation(){
+		return fRelation;
+	}
+
+	public void setRelation(Relation relation) {
+		fRelation = relation;
+	}
+
+	@Override
+	public boolean mentions(CategoryNode category){
+		return category == fCategory;
+	}
+
+	@Override
+	public boolean evaluate(List<PartitionNode> values) {
+		if(fCategory.getMethod() == null){
+			return false;
+		}
+		int index = fCategory.getMethod().getCategories().indexOf(fCategory);
+		boolean containsLabel = values.get(index).getAllLabels().contains(fConditionLabel); 
+		
+		switch (fRelation){
+		case EQUAL:
+			return containsLabel;
+		case NOT:
+			return !containsLabel;
+		default:
+			return false;
+		}
+	}
+	
+	@Override
+	public String toString(){
+		return fCategory.getName() + fRelation + fConditionLabel;
+	}
+}
