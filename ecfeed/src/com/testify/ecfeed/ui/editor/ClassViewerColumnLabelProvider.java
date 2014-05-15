e686abfb57fa8df6240e3c80d2f9f0f4e1d2598a
m.strozynski@radytek.com <m.strozynski@radytek.com>
2014-05-15 10:17:43 +0200

    Add class viewer column label decorator

---
+++ ecfeed/src/com/testify/ecfeed/ui/editor/ClassViewerColumnLabelProvider.java
@@ -0,0 +1,24 @@
+package com.testify.ecfeed.ui.editor;
+
+import org.eclipse.jface.viewers.ColumnLabelProvider;
+import org.eclipse.swt.graphics.Color;
+
+import com.testify.ecfeed.model.ClassNode;
+import com.testify.ecfeed.ui.common.ColorConstants;
+import com.testify.ecfeed.ui.common.ColorManager;
+import com.testify.ecfeed.utils.ModelUtils;
+
+public class ClassViewerColumnLabelProvider extends ColumnLabelProvider {
+	
+	private ColorManager fColorManager = new ColorManager();
+	
+	@Override
+	public Color getForeground(Object element) {
+		if (element instanceof ClassNode) {
+			if (ModelUtils.isClassImplemented((ClassNode)element)) {
+				return fColorManager.getColor(ColorConstants.CLASS_IMPLEMENTED);
+			}
+		}
+		return null;
+	}
+}
