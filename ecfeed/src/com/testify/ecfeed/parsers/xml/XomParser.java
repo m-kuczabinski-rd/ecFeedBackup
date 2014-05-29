fedf9d5dc484a51257a3fa5989960c73dcdf73e6
p.chamuczynski@radytek.com <p.chamuczynski@radytek.com>
2014-05-29 14:50:37 +0200

    Initial implementation of XomParser

---
+++ ecfeed/src/com/testify/ecfeed/parsers/xml/XomParser.java
@@ -0,0 +1,76 @@
+package com.testify.ecfeed.parsers.xml;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import nu.xom.Element;
+import nu.xom.Elements;
+import nu.xom.Node;
+import static com.testify.ecfeed.parsers.Constants.*;
+
+import com.testify.ecfeed.model.ClassNode;
+import com.testify.ecfeed.model.RootNode;
+import com.testify.ecfeed.parsers.Constants;
+import com.testify.ecfeed.parsers.ParserException;
+
+public class XomParser {
+	public RootNode parseRoot(Element element) throws ParserException{
+		assertNodeTag(element.getQualifiedName(), ROOT_NODE_NAME);
+		String name = getElementName(element);
+		
+		RootNode root = new RootNode(name);
+		
+		for(Element child : getIterableChildren(element)){
+			root.addClass(parseClass(child));
+		}
+		
+		return root;
+	}
+	
+	public ClassNode parseClass(Element element) throws ParserException{
+		assertNodeTag(element.getQualifiedName(), CLASS_NODE_NAME);
+		String name = getElementName(element);
+		
+		ClassNode _class = new ClassNode(name);
+		
+		return _class;
+	}
+
+
+
+	private void assertNodeTag(String qualifiedName, String expectedName) throws ParserException {
+		if(qualifiedName.equals(expectedName) == false){
+			throw new ParserException("Unexpected node name: " + qualifiedName + " instead of " + expectedName);
+		}
+	}
+	
+	protected List<Element> getIterableChildren(Element element){
+		ArrayList<Element> list = new ArrayList<Element>();
+		Elements children = element.getChildElements();
+		for(int i = 0; i < children.size(); i++){
+			Node node = children.get(i);
+			if(node instanceof Element){
+				list.add((Element)node);
+			}
+		}
+		return list;
+	}
+
+	protected String getElementName(Element element) throws ParserException {
+		String name = element.getAttributeValue(Constants.NODE_NAME_ATTRIBUTE);
+		if(name == null){
+			throw new ParserException(Messages.MISSING_ATTRIBUTE(element, Constants.NODE_NAME_ATTRIBUTE));
+		}
+		return name;
+	}
+
+	protected String getAttributeValue(Element element, String attributeName) throws ParserException{
+		String value = element.getAttributeValue(attributeName);
+		if(value == null){
+			throw new ParserException(Messages.MISSING_ATTRIBUTE(element, attributeName));
+		}
+		return value;
+	}
+
+
+}
