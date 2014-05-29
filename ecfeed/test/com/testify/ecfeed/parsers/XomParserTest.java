e133e95537067b703b27afa04a419295c1f2035a
p.chamuczynski@radytek.com <p.chamuczynski@radytek.com>
2014-05-29 15:21:55 +0200

    parser test for RootNode

--- ecfeed/test/com/testify/ecfeed/parsers/XomParserTest.java
+++ ecfeed/test/com/testify/ecfeed/parsers/XomParserTest.java
@@ -1,7 +1,14 @@
 package com.testify.ecfeed.parsers;
 
 import static org.junit.Assert.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import nu.xom.Document;
 import nu.xom.Element;
+import nu.xom.Serializer;
 
 import org.junit.Test;
 
@@ -12,6 +19,8 @@ import com.testify.ecfeed.testutils.RandomModelGenerator;
 
 public class XomParserTest {
 	
+	private final boolean DEBUG = true;
+	
 	RandomModelGenerator fModelGenerator = new RandomModelGenerator();
 	XomParser fParser = new XomParser();
 	
@@ -21,13 +30,36 @@ public class XomParserTest {
 		
 		Element rootElement = (Element)root.convert(new XomConverter());
 		
+		TRACE(rootElement);
+		
 		try {
 			RootNode parsedRoot = fParser.parseRoot(rootElement);
 			
 			assertTrue(parsedRoot.compare(root));
+
+			
+
 		} catch (ParserException e) {
 			fail("Unexpected exception: " + e.getMessage());
 		}
+	}
+	
+	private void TRACE(Element element){
+		if(!DEBUG) return;
+		
+		Document document = new Document(element);
+		OutputStream ostream = new ByteArrayOutputStream();
+		Serializer serializer = new Serializer(ostream);
+		// Uncomment for pretty formatting. This however will affect 
+		// whitespaces in the document's ... infoset
+		serializer.setIndent(4);
+		try {
+			serializer.write(document);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println(ostream);
 	}
 
 }
