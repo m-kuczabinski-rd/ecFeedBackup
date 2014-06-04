eb02e22839d5aea9718a15c354b9e07e45461a22
mgluszko <m.gluszko@radytek.com>
2014-06-04 15:31:19 +0200

    Applied copyrights

--- ecfeed/src/com/testify/ecfeed/ui/editor/MenuOperation.java
+++ ecfeed/src/com/testify/ecfeed/ui/editor/MenuOperation.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Testify AS.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Michal Gluszko (m.gluszko(at)radytek.com) - initial implementation
+ ******************************************************************************/
 package com.testify.ecfeed.ui.editor;
 
 public abstract class MenuOperation{
