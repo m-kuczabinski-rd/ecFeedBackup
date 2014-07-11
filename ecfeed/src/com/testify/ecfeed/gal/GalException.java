25d1e0873502f8f368100737b5c678b534e5528e
p.chamuczynski@radytek.com <p.chamuczynski@radytek.com>
2014-07-11 10:07:11 +0200

    introduced GalException

---
+++ ecfeed/src/com/testify/ecfeed/gal/GalException.java
@@ -0,0 +1,14 @@
+package com.testify.ecfeed.gal;
+
+public class GalException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2841889790004375884L;
+
+	public GalException(String message){
+		super(message);
+	}
+
+}
