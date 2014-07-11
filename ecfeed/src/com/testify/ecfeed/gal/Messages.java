ae6598414da0649691781d4ef9dfd2fce9e60f20
p.chamuczynski@radytek.com <p.chamuczynski@radytek.com>
2014-07-11 12:25:46 +0200

    new message

---
+++ ecfeed/src/com/testify/ecfeed/gal/Messages.java
@@ -0,0 +1,8 @@
+package com.testify.ecfeed.gal;
+
+public class Messages {
+
+	public static final String PARTITION_NAME_NOT_UNIQUE_PROBLEM = "Partition name must be unique within a category or parent partition";
+	public static final String PARTITION_NAME_REGEX_PROBLEM = "Partition name must contain between 1 and 64 characters and do not contain only white space characters";
+
+}
