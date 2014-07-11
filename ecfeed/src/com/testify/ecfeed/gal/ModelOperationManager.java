031a1f847ee6e6ecfe5bf4686a02672464f88203
p.chamuczynski@radytek.com <p.chamuczynski@radytek.com>
2014-07-11 12:23:35 +0200

    ModelOperationManager with undo/redo functionality

---
+++ ecfeed/src/com/testify/ecfeed/gal/ModelOperationManager.java
@@ -0,0 +1,42 @@
+package com.testify.ecfeed.gal;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ModelOperationManager {
+	private List<IModelOperation> fHistory;
+	private int fHistoryIndex = 0;
+	
+	public ModelOperationManager(){
+		fHistory = new ArrayList<IModelOperation>();
+	}
+	
+	protected void updateHistory(IModelOperation operation){
+		if(fHistory.size() > fHistoryIndex){
+			fHistory = fHistory.subList(0, fHistoryIndex);
+		}
+		else if(fHistory.size() < fHistoryIndex){
+			//shouldn't happen, but let's protect against it.
+			fHistoryIndex = fHistory.size();
+		}
+		
+		fHistory.add(operation);
+		++fHistoryIndex;
+	}
+	
+	public void undo() throws GalException{
+		if(fHistoryIndex > 0){
+			IModelOperation operation = fHistory.get(fHistoryIndex - 1).reverseOperation();
+			operation.execute();
+			--fHistoryIndex;
+		}
+	}
+	
+	public void redo() throws GalException{
+		if(fHistoryIndex <= fHistory.size()){
+			IModelOperation operation = fHistory.get(fHistoryIndex);
+			operation.execute();
+			++fHistoryIndex;
+		}
+	}
+}
