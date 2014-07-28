6f199762c1ee28e8d5e3098e8824891f1d6363de
p.chamuczynski@radytek.com <p.chamuczynski@radytek.com>
2014-07-28 15:00:39 +0200

    polished error message

--- ecfeed/src/com/testify/ecfeed/gal/java/partition/Messages.java
+++ ecfeed/src/com/testify/ecfeed/gal/java/partition/Messages.java
@@ -5,8 +5,8 @@ public class Messages {
 	public static final String PARTITION_NAME_NOT_UNIQUE_PROBLEM = "Partition name must be unique within a category or parent partition";
 	public static final String PARTITION_NAME_REGEX_PROBLEM = "Partition name must contain between 1 and 64 characters and do not contain only white space characters";
 	public static final String PARTITION_VALUE_PROBLEM(String value){
-		return "Value " + value + " is not valid for given category.\n" + 
-				"Partition value must fit to type and range of the represented variable.\n" +
+		return "Value " + value + " is not valid for given parameter.\n\n" + 
+				"Partition value must fit to type and range of the represented parameter.\n" +
 				"Partitions of user defined type must follow Java enum defining rules.";
 	}
 
