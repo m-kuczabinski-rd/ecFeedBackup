e6d2a587d36071faeaa4bcbc7ab844558191dec7
p.chamuczynski@radytek.com <p.chamuczynski@radytek.com>
2014-07-29 14:28:02 +0200

    added new messages file in modelif

---
+++ ecfeed/src/com/testify/ecfeed/ui/modelif/Messages.java
@@ -0,0 +1,7 @@
+package com.testify.ecfeed.ui.modelif;
+
+public class Messages {
+	public static final String DIALOG_RENAME_MODEL_PROBLEM_TITLE = "Couldn't rename model";
+	public static final String DIALOG_ADD_NEW_CLASS_PROBLEM_TITLE = "Couldn't add new class";
+
+}
