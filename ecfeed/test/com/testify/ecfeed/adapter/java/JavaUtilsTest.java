26132a6066981510738ea0d97bbc1d4a5a77f09e
p.chamuczynski@radytek.com <p.chamuczynski@radytek.com>
2014-10-22 14:50:36 +0200

    tests for type name validation

--- ecfeed/test/com/testify/ecfeed/adapter/java/JavaUtilsTest.java
+++ ecfeed/test/com/testify/ecfeed/adapter/java/JavaUtilsTest.java
@@ -1,5 +1,35 @@
 package com.testify.ecfeed.adapter.java;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.testify.ecfeed.generators.CartesianProductGenerator;
+import com.testify.ecfeed.junit.OnlineRunner;
+import com.testify.ecfeed.junit.annotations.Constraints;
+import com.testify.ecfeed.junit.annotations.EcModel;
+import com.testify.ecfeed.junit.annotations.Generator;
+
+@RunWith(OnlineRunner.class)
+@EcModel("test/com.testify.ecfeed.adapter.java.ect")
+@Generator(CartesianProductGenerator.class)
+@Constraints(Constraints.ALL)
 public class JavaUtilsTest {
+
+	@Test
+	public void isValidTypeNameTest(String packageName, String className, boolean valid){
+		// TODO Auto-generated method stub
+		System.out.println("isValidTypeNameTest(" + packageName + ", " + className + ", " + valid + ")");
+		String typeName;
+		if(packageName.length() > 0){
+			typeName = packageName + "." + className;
+		}
+		else{
+			typeName = className;
+		}
+		boolean result = JavaUtils.isValidTypeName(typeName);
+		assertEquals(valid, result);
+	}
 
 }
