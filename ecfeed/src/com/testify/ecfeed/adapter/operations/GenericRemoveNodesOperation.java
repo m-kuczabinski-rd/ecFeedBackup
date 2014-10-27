fee574267592ea5240b94f1851b765c60ffac92b
p.chamuczynski@radytek.com <p.chamuczynski@radytek.com>
2014-10-27 16:35:59 +0100

    fixed removing tree node twice (once explicitly and once with parent)

--- ecfeed/src/com/testify/ecfeed/adapter/operations/GenericRemoveNodesOperation.java
+++ ecfeed/src/com/testify/ecfeed/adapter/operations/GenericRemoveNodesOperation.java
@@ -1,14 +1,32 @@
 package com.testify.ecfeed.adapter.operations;
 
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 import com.testify.ecfeed.model.GenericNode;
 
 public class GenericRemoveNodesOperation extends BulkOperation {
 
+	private Set<GenericNode> fRemoved;
+	
 	public GenericRemoveNodesOperation(Collection<? extends GenericNode> nodes, boolean validate){
 		super(OperationNames.REMOVE_NODES, false);
-		for(GenericNode node : nodes){
+		fRemoved = new HashSet<>(nodes);
+		Iterator<GenericNode> it = fRemoved.iterator();
+		while(it.hasNext()){
+			GenericNode node = it.next();
+			for(GenericNode ancestor : node.getAncestors()){
+				if(fRemoved.contains(ancestor)){
+					it.remove();
+					break;
+				}
+			}
+		}
+		
+		
+		for(GenericNode node : fRemoved){
 			addOperation(FactoryRemoveOperation.getRemoveOperation(node, validate));
 		}
 	}
