6d01e155d6c1e03e3a3ed5ec8acf642fc54512e0
mgluszko <m.gluszko@radytek.com>
2014-11-06 13:52:15 +0100

    In progress.

--- ecfeed/test/com/testify/ecfeed/spec/gui/ImplementationStatus.java
+++ ecfeed/test/com/testify/ecfeed/spec/gui/ImplementationStatus.java
@@ -2,5 +2,31 @@ package com.testify.ecfeed.spec.gui;
 
 public class ImplementationStatus{
 
+	public void implementedPartitionTest(boolean primitive, boolean implementedValue, EChildrenStatus childrenStatus){
+		// TODO Auto-generated method stub
+		System.out.println("implementedPartitionTest(" + primitive + ", " + implementedValue + ", " + childrenStatus + ")");
+	}
+
+	public void implementedCategoryTest(boolean primitive, boolean implementedValue, EChildrenStatus childrenStatus){
+		// TODO Auto-generated method stub
+		System.out.println("implementedCategoryTest(" + primitive + ", " + implementedValue + ", " + childrenStatus + ")");
+	}
+
+	public void implementedMethodTest(EChildrenStatus childrenStatus, boolean hasImplementation){
+		// TODO Auto-generated method stub
+		System.out.println("implementedMethodTest(" + childrenStatus + ", " + hasImplementation + ")");
+	}
+
+	public void implementedClassTest(EChildrenStatus childrenStatus, boolean hasImplementation){
+		// TODO Auto-generated method stub
+		System.out.println("implementedClassTest(" + childrenStatus + ", " + hasImplementation + ")");
+	}
+
+	public void implementedTestCaseTest(EChildrenStatus childrenStatus){
+		// TODO Auto-generated method stub
+		System.out.println("implementedTestCaseTest(" + childrenStatus + ")");
+	}
+
+
 }
 
---
+++ ecfeed/test/com/testify/ecfeed/spec/gui/NewFileWizard.java
@@ -0,0 +1,16 @@
+package com.testify.ecfeed.spec.gui;
+
+public class NewFileWizard{
+
+	public void createModelTest(String modelName){
+		// TODO Auto-generated method stub
+		System.out.println("createModelTest(" + modelName + ")");
+	}
+
+	public void defaultFileName(){
+		// TODO Auto-generated method stub
+		System.out.println("defaultFileName()");
+	}
+
+}
+
