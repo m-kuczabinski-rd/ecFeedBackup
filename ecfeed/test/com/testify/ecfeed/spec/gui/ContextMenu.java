4a1d55c35e4ed056aa1f516687ae19d3703eb8b9
mgluszko <m.gluszko@radytek.com>
2014-11-17 13:21:28 +0100

    Fixed Issue-157.

--- ecfeed/test/com/testify/ecfeed/spec/gui/ContextMenu.java
+++ ecfeed/test/com/testify/ecfeed/spec/gui/ContextMenu.java
@@ -1,25 +1,27 @@
 package com.testify.ecfeed.spec.gui;
 
+import com.testify.ecfeed.testutils.ENodeType;
+
 public class ContextMenu{
 
-	public void addChildTest(){
+	public void addChildTest(ENodeType nodeType, ESelectionType selectionType){
 		// TODO Auto-generated method stub
-		System.out.println("addChildTest()");
+		System.out.println("addChildTest(" + nodeType + ", " + selectionType + ")");
 	}
 
-	public void copyTest(){
+	public void copyTest(ENodeType sourceType, ESelectionType selectionType){
 		// TODO Auto-generated method stub
-		System.out.println("copyTest()");
+		System.out.println("copyTest(" + sourceType + ", " + selectionType + ")");
 	}
 
-	public void pasteTest(){
+	public void pasteSelectionPossibilityTest(ESelectionType selectionType, ENodeType targetType){
 		// TODO Auto-generated method stub
-		System.out.println("pasteTest()");
+		System.out.println("pasteSelectionPossibilityTest(" + selectionType + ", " + targetType + ")");
 	}
 
-	public void cutTest(){
+	public void pastePossibilityTest(ENodeType targetType, ENodeType sourceType){
 		// TODO Auto-generated method stub
-		System.out.println("cutTest()");
+		System.out.println("pastePossibilityTest(" + targetType + ", " + sourceType + ")");
 	}
 
 	public void deleteTest(){
@@ -40,6 +42,26 @@ public class ContextMenu{
 	public void MoveUpDownTest(){
 		// TODO Auto-generated method stub
 		System.out.println("MoveUpDownTest()");
+	}
+
+	public void pasteDuplicateNameTest(ENodeType nodeType, boolean duplicateName){
+		// TODO Auto-generated method stub
+		System.out.println("pasteDuplicateNameTest(" + nodeType + ", " + duplicateName + ")");
+	}
+
+	public void pasteBulkAtomicTest(ENodeType nodeType, ENodeValidationStatus validNodes){
+		// TODO Auto-generated method stub
+		System.out.println("pasteBulkAtomicTest(" + nodeType + ")");
+	}
+
+	public void pasteNodesTest(ENodeType sourceType){
+		// TODO Auto-generated method stub
+		System.out.println("pasteNodesTest(" + sourceType + ")");
+	}
+
+	public void cutTest(ENodeType nodeType, ESelectionType selectionType){
+		// TODO Auto-generated method stub
+		System.out.println("cutTest(" + nodeType + ", " + selectionType + ")");
 	}
 
 }
--- ecfeed/test/com/testify/ecfeed/spec/gui/ImplementationStatus.java
+++ ecfeed/test/com/testify/ecfeed/spec/gui/ImplementationStatus.java
@@ -2,32 +2,32 @@ package com.testify.ecfeed.spec.gui;
 
 public class ImplementationStatus{
 
-	public void implementedPartitionTest(boolean primitive, boolean implementedValue, EChildrenStatus childrenStatus){
+	public void implementedPartitionTest(boolean primitive, boolean implementedValue, EChildrenImplementationStatus childrenStatus){
 		// TODO Auto-generated method stub
 		System.out.println("implementedPartitionTest(" + primitive + ", " + implementedValue + ", " + childrenStatus + ")");
 	}
 
-	public void implementedCategoryTest(boolean primitive, boolean implementedValue, EChildrenStatus childrenStatus){
+	public void implementedCategoryTest(boolean primitive, boolean implementedValue, EChildrenImplementationStatus childrenStatus){
 		// TODO Auto-generated method stub
 		System.out.println("implementedCategoryTest(" + primitive + ", " + implementedValue + ", " + childrenStatus + ")");
 	}
 
-	public void implementedMethodTest(EChildrenStatus childrenStatus, boolean hasImplementation){
+	public void implementedMethodTest(EChildrenImplementationStatus childrenStatus, boolean hasImplementation){
 		// TODO Auto-generated method stub
 		System.out.println("implementedMethodTest(" + childrenStatus + ", " + hasImplementation + ")");
 	}
 
-	public void implementedClassTest(EChildrenStatus childrenStatus, boolean hasImplementation){
+	public void implementedClassTest(EChildrenImplementationStatus childrenStatus, boolean hasImplementation){
 		// TODO Auto-generated method stub
 		System.out.println("implementedClassTest(" + childrenStatus + ", " + hasImplementation + ")");
 	}
 
-	public void implementedTestCaseTest(EChildrenStatus childrenStatus){
+	public void implementedTestCaseTest(EChildrenImplementationStatus childrenStatus){
 		// TODO Auto-generated method stub
 		System.out.println("implementedTestCaseTest(" + childrenStatus + ")");
 	}
 
-	public void implementedRootTest(EChildrenStatus childrenStatus){
+	public void implementedRootTest(EChildrenImplementationStatus childrenStatus){
 		// TODO Auto-generated method stub
 		System.out.println("implementedRootTest(" + childrenStatus + ")");
 	}
