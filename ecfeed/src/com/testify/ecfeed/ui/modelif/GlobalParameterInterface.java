bb40739ee453c774b5eec49536cc3ed14d1f7465
p.chamuczynski@radytek.com <p.chamuczynski@radytek.com>
2014-12-09 14:13:59 +0100

    global parameter operation set type

--- ecfeed/src/com/testify/ecfeed/ui/modelif/GlobalParameterInterface.java
+++ ecfeed/src/com/testify/ecfeed/ui/modelif/GlobalParameterInterface.java
@@ -2,15 +2,21 @@ package com.testify.ecfeed.ui.modelif;
 
 import java.util.List;
 
+import com.testify.ecfeed.adapter.ITypeAdapterProvider;
+import com.testify.ecfeed.adapter.operations.GlobalParameterOperationSetType;
 import com.testify.ecfeed.model.GlobalParameterNode;
 import com.testify.ecfeed.model.MethodParameterNode;
+import com.testify.ecfeed.ui.common.EclipseTypeAdapterProvider;
+import com.testify.ecfeed.ui.common.Messages;
 
 public class GlobalParameterInterface extends AbstractParameterInterface {
 
 	private GlobalParameterNode fTarget;
+	private ITypeAdapterProvider fAdapterProvider;
 
 	public GlobalParameterInterface(IModelUpdateContext updateContext) {
 		super(updateContext);
+		fAdapterProvider = new EclipseTypeAdapterProvider();
 	}
 
 	public void setTarget(GlobalParameterNode target){
@@ -20,5 +26,13 @@ public class GlobalParameterInterface extends AbstractParameterInterface {
 
 	public List<MethodParameterNode> getLinkers(){
 		return fTarget.getLinkers();
+	}
+
+	@Override
+	public boolean setType(String newType) {
+		if(newType.equals(fTarget.getType())){
+			return false;
+		}
+		return execute(new GlobalParameterOperationSetType(fTarget, newType, fAdapterProvider), Messages.DIALOG_RENAME_PAREMETER_PROBLEM_TITLE);
 	}
 }
