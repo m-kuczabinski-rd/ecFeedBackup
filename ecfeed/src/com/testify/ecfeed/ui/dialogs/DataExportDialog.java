0305ff639a2e2bb47b79361e52b50a5947f8f818
Patryk Chamuczyński <p.chamuczynski@radytek.com>
2015-09-25 14:55:31 +0200

    initial implementation of export dialog

--- ecfeed/src/com/testify/ecfeed/ui/dialogs/DataExportDialog.java
+++ ecfeed/src/com/testify/ecfeed/ui/dialogs/DataExportDialog.java
@@ -1,13 +1,13 @@
 package com.testify.ecfeed.ui.dialogs;
 
-import java.awt.Label;
-
 import org.eclipse.jface.dialogs.TitleAreaDialog;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 
@@ -15,12 +15,14 @@ import com.testify.ecfeed.ui.common.Messages;
 
 public class DataExportDialog extends TitleAreaDialog{
 
-	private String fPrefaceTemplate;
+	private String fHeaderTemplate;
 	private String fTestCaseTemplate;
 	private String fTailTemplate;
 	private Text fHeaderTemplateText;
 	private Text fTestCaseTemplateText;
 	private Text fTailTemplateText;
+	private Text fTargetFileText;
+	private String fTargetFile;
 	
 	public DataExportDialog(Shell parentShell) {
 		super(parentShell);
@@ -54,27 +56,69 @@ public class DataExportDialog extends TitleAreaDialog{
 	}
 
 	private void createTemplateHeaderComposite(Composite parent) {
-		new Label(Messages.EXPORT_TEST_DATA_HEADER_TEMPLATE_LABEL);
+		Label label = new Label(parent, SWT.NONE);
+		label.setText(Messages.EXPORT_TEST_DATA_HEADER_TEMPLATE_LABEL);
 
 		fHeaderTemplateText = new Text(parent, SWT.WRAP|SWT.MULTI|SWT.BORDER|SWT.V_SCROLL);
 		fHeaderTemplateText.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
 	}
 
 	private void createTestCaseTemplateComposite(Composite parent) {
-		new Label(Messages.EXPORT_TEST_DATA_TEST_CASE_TEMPLATE_LABEL);
+		Label label = new Label(parent, SWT.NONE);
+		label.setText(Messages.EXPORT_TEST_DATA_TEST_CASE_TEMPLATE_LABEL);
 		
 		fTestCaseTemplateText = new Text(parent, SWT.WRAP|SWT.MULTI|SWT.BORDER|SWT.V_SCROLL);
 		fTestCaseTemplateText.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
 	}
 
 	private void createTemplateTailComposite(Composite parent) {
-		new Label(Messages.EXPORT_TEST_DATA_TAIL_TEMPLATE_LABEL);
+		Label label = new Label(parent, SWT.NONE);
+		label.setText(Messages.EXPORT_TEST_DATA_TAIL_TEMPLATE_LABEL);
 		
 		fTailTemplateText = new Text(parent, SWT.WRAP|SWT.MULTI|SWT.BORDER|SWT.V_SCROLL);
 		fTailTemplateText.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
 	}
 
-	private void createTargetFileContainer(Composite container) {
+	private void createTargetFileContainer(Composite parent) {
+		Composite container = new Composite(parent, SWT.NONE);
+		container.setLayout(new GridLayout(1, false));
+		container.setLayoutData(new GridData(GridData.FILL_BOTH));
+
+		Label targetFileLabel = new Label(parent, SWT.NONE); 
+		targetFileLabel.setText(Messages.EXPORT_TEST_DATA_TARGET_FILE_LABEL);
+
+		Composite targetFileContainer = new Composite(parent, SWT.NONE);
+		targetFileContainer.setLayout(new GridLayout(2, false));
+		targetFileContainer.setLayoutData(new GridData(GridData.FILL_BOTH));
+		fTargetFileText = new Text(targetFileContainer, SWT.BORDER);
 		
+		Button browseButton = new Button(targetFileContainer, SWT.NONE);
+		browseButton.setText("Browse...");
 	}
+	
+	@Override
+	protected void okPressed(){
+		fHeaderTemplate = fHeaderTemplateText.getText();
+		fTestCaseTemplate = fTestCaseTemplateText.getText();
+		fTailTemplate = fTailTemplateText.getText();
+		fTargetFile = fTargetFileText.getText();
+		super.okPressed();
+	}
+
+	public String getHeaderTemplate(){
+		return fHeaderTemplate;
+	}
+
+	public String getTestCaseTemplate(){
+		return fTestCaseTemplate;
+	}
+
+	public String getTailTemplate(){
+		return fTailTemplate;
+	}
+
+	public String getTargetFile(){
+		return fTargetFile;
+	}
+
 }
