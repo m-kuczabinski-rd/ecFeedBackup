5a30e30341952285d748823a0fdb496860cd4a94
Marek <m.kuczabinski@radytek.com>
2016-01-29 10:02:01 +0100

    Copyrights added.

--- ecfeed.rcp3/src/com/testify/ecfeed/rcp3/Application.java
+++ ecfeed.rcp3/src/com/testify/ecfeed/rcp3/Application.java
@@ -1,3 +1,11 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Testify AS..
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ ******************************************************************************/
+
 package com.testify.ecfeed.rcp3;
 
 import org.eclipse.equinox.app.IApplication;
@@ -8,14 +16,8 @@ import org.eclipse.ui.PlatformUI;
 
 import com.testify.ecfeed.application.ApplicationContext;
 
-/**
- * This class controls all aspects of the application's execution
- */
 public class Application implements IApplication {
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.equinox.app.IApplication#start(org.eclipse.equinox.app.IApplicationContext)
-	 */
 	public Object start(IApplicationContext context) {
 		ApplicationContext.setStandaloneApplication();
 		
@@ -31,9 +33,6 @@ public class Application implements IApplication {
 		}
 	}
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.equinox.app.IApplication#stop()
-	 */
 	public void stop() {
 		if (!PlatformUI.isWorkbenchRunning())
 			return;
