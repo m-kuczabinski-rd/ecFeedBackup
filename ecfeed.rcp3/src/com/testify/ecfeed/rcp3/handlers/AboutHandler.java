49931f9f9baebae806135bb1ce4f40d0bfd8747e
Marek <m.kuczabinski@radytek.com>
2016-02-04 11:04:14 +0100

    About dialog added.

--- ecfeed.rcp3/src/com/testify/ecfeed/rcp3/handlers/AboutHandler.java
+++ ecfeed.rcp3/src/com/testify/ecfeed/rcp3/handlers/AboutHandler.java
@@ -10,6 +10,9 @@ package com.testify.ecfeed.rcp3.handlers;
 
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.dialogs.MessageDialog;
+
+import com.testify.ecfeed.utils.EclipseHelper;
 
 
 public class AboutHandler extends org.eclipse.core.commands.AbstractHandler {
@@ -17,6 +20,12 @@ public class AboutHandler extends org.eclipse.core.commands.AbstractHandler {
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		System.out.println("About handler executed");
+		
+		MessageDialog.openInformation(
+				EclipseHelper.getActiveShell(), 
+				"About ecFeed", 
+				"EcFeed is a tool which allows to design and model tests.");
+		
 		return null;
 	}
 }
