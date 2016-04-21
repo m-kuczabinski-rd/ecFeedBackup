02711c867161eee7477e358dde3d4b096732ca12
Marek Kuczabinski <m.kuczabinski@radytek.com>
2016-04-21 14:42:48 +0200

    TestCasesExportDialog. Non member functions extracted to helper class.

--- ecfeed/src/com/testify/ecfeed/ui/dialogs/TestCasesExportDialog.java
+++ ecfeed/src/com/testify/ecfeed/ui/dialogs/TestCasesExportDialog.java
@@ -27,23 +27,9 @@ import org.osgi.framework.Bundle;
 import org.osgi.framework.FrameworkUtil;
 
 import com.testify.ecfeed.core.utils.StringHelper;
+import com.testify.ecfeed.ui.dialogs.basic.ExceptionCatchDialog;
 
 public class TestCasesExportDialog extends TitleAreaDialog {
-
-	private static final String SECTION_HEADER_REGEX = "\\s*\\[([^]]*)\\]\\s*";
-	private static final String COMMENTED_LINE_REGEX = "^\\s*#.*";
-	private static final String DEFAULT_TEMPLATE_TEXT_FILE = "res/TestCasesExportTemplate.txt";
-
-
-	private final String HEADER_TEMPLATE_MARKER = "[Header]";
-	private final String TEST_CASE_TEMPLATE_MARKER = "[TestCase]";
-	private final String FOOTER_TEMPLATE_MARKER = "[Footer]";
-
-	public static final String DIALOG_EXPORT_TEST_DATA_TITLE = "Export test data";
-	public static final String DIALOG_EXPORT_TEST_DATA_MESSAGE = "Define template for data export and select target file";
-	public static final String MSG_SELECT_TARGET = "Select target export file";
-	public static final String EXPORT_TEST_DATA_TEMPLATE_LABEL = "Define template for export data.";
-	public static final String EXPORT_TEST_DATA_TARGET_FILE_LABEL = "Target file";
 
 	private int fMethodParametersCount;
 	private String fHeaderTemplate;
@@ -53,29 +39,20 @@ public class TestCasesExportDialog extends TitleAreaDialog {
 	private Text fTargetFileText;
 	private String fTargetFile;
 
-	class BrowseAdapter extends SelectionAdapter{
-		@Override
-		public void widgetSelected(SelectionEvent e){
-			FileDialog dialog = new FileDialog(getParentShell());
-			fTargetFileText.setText(dialog.open());
-		}
-	}
-
 	public TestCasesExportDialog(Shell parentShell, int methodParametersCount) {
 		super(parentShell);
 		fMethodParametersCount = methodParametersCount;
 	}
 
+	public static boolean isExtendedMode() {
+		return false;
+	}
+
 	@Override
 	protected Control createDialogArea(Composite parent) {
-		setTitle(DIALOG_EXPORT_TEST_DATA_TITLE);
-
-		if (isExtendedMode()) {
-			setMessage(DIALOG_EXPORT_TEST_DATA_MESSAGE);
-		} else {
-			setMessage(MSG_SELECT_TARGET);
-		}
-
+		setDialogTitle(this);
+		setDialogMessage(this);
+		
 		Composite area = (Composite) super.createDialogArea(parent);
 		Composite container = new Composite(area, SWT.NONE);
 		container.setLayout(new GridLayout(1, false));
@@ -88,11 +65,22 @@ public class TestCasesExportDialog extends TitleAreaDialog {
 		createTargetFileContainer(container);
 		return area;
 	}
-
-	private boolean isExtendedMode() {
-		return false;
+	
+	public void setDialogTitle(TitleAreaDialog dialog) {
+		final String EXPORT_TEST_DATA_TITLE = "Export test data";
+		setTitle(EXPORT_TEST_DATA_TITLE);
 	}
 
+	public void setDialogMessage(TitleAreaDialog dialog)	{
+		if (isExtendedMode()) {
+			final String EXPORT_TEST_DATA_MESSAGE = "Define template for data export and select target file";
+			setMessage(EXPORT_TEST_DATA_MESSAGE);
+		} else {
+			final String SELECT_TARGET = "Select target export file";
+			setMessage(SELECT_TARGET);
+		}
+	}
+	
 	private void createTemplateDefinitionContainer(Composite parent) {
 		Composite container = new Composite(parent, SWT.NONE);
 		container.setLayout(new GridLayout(1, false));
@@ -103,6 +91,8 @@ public class TestCasesExportDialog extends TitleAreaDialog {
 
 	private void createTemplateComposite(Composite parent) {
 		Label label = new Label(parent, SWT.NONE);
+		
+		final String EXPORT_TEST_DATA_TEMPLATE_LABEL = "Define template for export data.";
 		label.setText(EXPORT_TEST_DATA_TEMPLATE_LABEL);
 
 		fTemplateText = new Text(parent, SWT.WRAP|SWT.MULTI|SWT.BORDER|SWT.V_SCROLL);
@@ -110,24 +100,8 @@ public class TestCasesExportDialog extends TitleAreaDialog {
 		gd.minimumHeight = 300;
 		fTemplateText.setLayoutData(gd);
 
-		fillTemplateText(DEFAULT_TEMPLATE_TEXT_FILE);
-	}
-
-	private void fillTemplateText(String templateFilePath) {
-		Bundle bundle = FrameworkUtil.getBundle(this.getClass());
-		URL url = FileLocator.find(bundle, new Path(templateFilePath), null);
-		BufferedReader in;
-		String templateText = "";
-		try {
-			in = new BufferedReader(new InputStreamReader(url.openStream()));
-			String inputLine;
-			while ((inputLine = in.readLine()) != null){
-				templateText += inputLine + "\n";
-			}
-			in.close();
-		} catch (IOException e) {
-		}
-		fTemplateText.setText(templateText);
+		final String DEFAULT_TEMPLATE_TEXT_FILE = "res/TestCasesExportTemplate.txt";
+		fTemplateText.setText(Helper.createTemplateText(this.getClass(), DEFAULT_TEMPLATE_TEXT_FILE));
 	}
 
 	private void createTargetFileContainer(Composite parent) {
@@ -135,7 +109,9 @@ public class TestCasesExportDialog extends TitleAreaDialog {
 		container.setLayout(new GridLayout(1, false));
 		container.setLayoutData(new GridData(GridData.FILL_BOTH));
 
-		Label targetFileLabel = new Label(parent, SWT.NONE); 
+		Label targetFileLabel = new Label(parent, SWT.NONE);
+		
+		final String EXPORT_TEST_DATA_TARGET_FILE_LABEL = "Target file";
 		targetFileLabel.setText(EXPORT_TEST_DATA_TARGET_FILE_LABEL);
 
 		Composite targetFileContainer = new Composite(parent, SWT.NONE);
@@ -153,51 +129,74 @@ public class TestCasesExportDialog extends TitleAreaDialog {
 	protected void okPressed(){
 		if (isExtendedMode()) {
 			String templateText = fTemplateText.getText();
-			Map<String, String> templates = parseTemplate(templateText);
+			Map<String, String> template = Helper.parseTemplate(templateText);
 
-			fHeaderTemplate = StringHelper.removeLastNewline(templates.get(HEADER_TEMPLATE_MARKER.toLowerCase()));
-			fTestCaseTemplate = StringHelper.removeLastNewline(templates.get(TEST_CASE_TEMPLATE_MARKER.toLowerCase()));
-			fFooterTemplate = StringHelper.removeLastNewline(templates.get(FOOTER_TEMPLATE_MARKER.toLowerCase()));
+			fHeaderTemplate = Helper.createUserHeaderTemplate(template);
+			fTestCaseTemplate = Helper.createUserTestCaseTemplate(template);
+			fFooterTemplate = Helper.createUserFooterTemplate(template);
 		} else {
-			fHeaderTemplate = createDefaultHeaderTemplate();
-			fTestCaseTemplate = createDefaultTestCaseTemplate();
-			fFooterTemplate = createDefaultFooterTemplate();
+			fHeaderTemplate = Helper.createDefaultHeaderTemplate(fMethodParametersCount);
+			fTestCaseTemplate = Helper.createDefaultTestCaseTemplate(fMethodParametersCount);
+			fFooterTemplate = Helper.createDefaultFooterTemplate();
 		}
 
 		fTargetFile = fTargetFileText.getText();
-
 		super.okPressed();
 	}
 
-	private String createDefaultHeaderTemplate() {
-		final String NAME_TAG = "name";
-		return createParameterTemplate(fMethodParametersCount, NAME_TAG);
+	public String getHeaderTemplate(){
+		return fHeaderTemplate;
 	}
 
-	private String createDefaultTestCaseTemplate() {
-		final String VALUE_TAG = "value";
-		return createParameterTemplate(fMethodParametersCount, VALUE_TAG);
+	public String getTestCaseTemplate(){
+		return fTestCaseTemplate;
 	}
 
-	private String createDefaultFooterTemplate() {
-		return new String();
+	public String getFooterTemplate(){
+		return fFooterTemplate;
 	}
 
-	private String createParameterTemplate(int parameterCount, String parameterTag) {
-		String template = new String();
-
-		for (int cnt = 1; cnt <= parameterCount; ++cnt) {
-			if (cnt > 1) {
-				template = template + ",";
-			}
-			String paramDescription = "$" + cnt + "." + parameterTag;
-			template = template + paramDescription;
+	public String getTargetFile(){
+		return fTargetFile;
+	}
+	
+	class BrowseAdapter extends SelectionAdapter{
+		@Override
+		public void widgetSelected(SelectionEvent e){
+			FileDialog dialog = new FileDialog(getParentShell());
+			fTargetFileText.setText(dialog.open());
 		}
+	}
+}
 
-		return template;
+class Helper
+{
+	public static String createTemplateText(
+			@SuppressWarnings("rawtypes") Class theClass, 
+			String templateFilePath) {
+		Bundle bundle = FrameworkUtil.getBundle(theClass);
+		URL url = FileLocator.find(bundle, new Path(templateFilePath), null);
+		BufferedReader in;
+		String templateText = "";
+		try {
+			in = new BufferedReader(new InputStreamReader(url.openStream()));
+			String inputLine;
+			while ((inputLine = in.readLine()) != null){
+				templateText += inputLine + "\n";
+			}
+			in.close();
+		} catch (IOException e) {
+			ExceptionCatchDialog.display("Can not read template", e.getMessage());
+		}
+		
+		return templateText;
 	}
 
-	private Map<String, String> parseTemplate(String templateText) {
+	public static Map<String, String> parseTemplate(String templateText) {
+		
+		final String SECTION_HEADER_REGEX = "\\s*\\[([^]]*)\\]\\s*";
+		final String COMMENTED_LINE_REGEX = "^\\s*#.*";
+		
 		Map<String, String> result = new HashMap<String, String>();
 		StringTokenizer tokenizer = new StringTokenizer(templateText, "\n");
 		String currentSection = "";
@@ -217,20 +216,49 @@ public class TestCasesExportDialog extends TitleAreaDialog {
 		}
 		return result;
 	}
-
-	public String getHeaderTemplate(){
-		return fHeaderTemplate;
+	
+	public static String createUserHeaderTemplate(Map<String, String> template)
+	{
+		final String HEADER_TEMPLATE_MARKER = "[Header]";
+		return StringHelper.removeLastNewline(template.get(HEADER_TEMPLATE_MARKER.toLowerCase()));
+	}
+	
+	public static String createUserTestCaseTemplate(Map<String, String> template) {
+		final String TEST_CASE_TEMPLATE_MARKER = "[TestCase]";
+		return StringHelper.removeLastNewline(template.get(TEST_CASE_TEMPLATE_MARKER.toLowerCase()));
+	}
+	
+	public static String createUserFooterTemplate(Map<String, String> template) {
+		final String FOOTER_TEMPLATE_MARKER = "[Footer]";
+		return StringHelper.removeLastNewline(template.get(FOOTER_TEMPLATE_MARKER.toLowerCase()));
 	}
 
-	public String getTestCaseTemplate(){
-		return fTestCaseTemplate;
+	public static String createDefaultHeaderTemplate(int paramCount) {
+		final String NAME_TAG = "name";
+		return Helper.createParameterTemplate(paramCount, NAME_TAG);
 	}
 
-	public String getFooterTemplate(){
-		return fFooterTemplate;
+	public static String createDefaultTestCaseTemplate(int paramCount) {
+		final String VALUE_TAG = "value";
+		return Helper.createParameterTemplate(paramCount, VALUE_TAG);
 	}
 
-	public String getTargetFile(){
-		return fTargetFile;
+	public static String createDefaultFooterTemplate() {
+		return new String();
 	}
+	
+	public static String createParameterTemplate(int parameterCount, String parameterTag) {
+		String template = new String();
+
+		for (int cnt = 1; cnt <= parameterCount; ++cnt) {
+			if (cnt > 1) {
+				template = template + ",";
+			}
+			String paramDescription = "$" + cnt + "." + parameterTag;
+			template = template + paramDescription;
+		}
+
+		return template;
+	}
+	
 }
