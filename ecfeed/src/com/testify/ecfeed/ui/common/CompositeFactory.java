3e06579ed72cef729ea27421719ebb31fe10e66d
Marek Kuczabinski <m.kuczabinski@radytek.com>
2016-04-22 12:27:03 +0200

    DialogHelper converted to CompositeFactory.

---
+++ ecfeed/src/com/testify/ecfeed/ui/common/CompositeFactory.java
@@ -0,0 +1,74 @@
+package com.testify.ecfeed.ui.common;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+
+public class CompositeFactory {
+	private static CompositeFactory instance = null;
+
+	protected CompositeFactory() {
+	}
+	
+	public static CompositeFactory getInstance() {
+		if (instance == null) {
+			instance = new CompositeFactory();
+		}
+		return instance;
+	}
+
+	public Composite createGridContainer(Composite parent, int countOfColumns) {
+
+		Composite container = new Composite(parent, SWT.NONE);
+
+		container.setLayout(new GridLayout(countOfColumns, false));
+		container.setLayoutData(new GridData(GridData.FILL_BOTH));
+
+		return container;
+	}
+
+	public Text createText(Composite parent, int minimumHeight, String initialText) {
+		Text templateText = new Text(parent, SWT.WRAP|SWT.MULTI|SWT.BORDER|SWT.V_SCROLL);
+
+		GridData gridData = new GridData(SWT.FILL, SWT.FILL, true, true);
+		gridData.minimumHeight = minimumHeight;
+		templateText.setLayoutData(gridData);
+
+		if (initialText != null) {
+			templateText.setText(initialText);
+		}
+
+		return templateText;
+	}
+
+	public Label createLabel(Composite parent, String text) {
+		Label label = new Label(parent, SWT.NONE);
+		label.setText(text);
+		return label;
+	}
+
+	public Text createFileSelectionText(Composite targetFileContainer) {
+		Text targetFileText = new Text(targetFileContainer, SWT.BORDER);
+		targetFileText.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false));
+		return targetFileText;
+	}
+
+	public Button createButton(Composite parent, String buttonText,SelectionListener selectionListener) {
+		Button browseButton = new Button(parent, SWT.NONE);
+		browseButton.setText(buttonText);
+		browseButton.addSelectionListener(selectionListener);
+
+		return browseButton;
+	}
+
+	public Button createBrowseButton(Composite parent, SelectionListener selectionListener) {
+		final String BROWSE_LABEL = "Browse...";
+		return createButton(parent, BROWSE_LABEL, selectionListener);
+	}
+
+}
--- ecfeed/src/com/testify/ecfeed/ui/dialogs/TestCasesExportDialog.java
+++ ecfeed/src/com/testify/ecfeed/ui/dialogs/TestCasesExportDialog.java
@@ -1,22 +1,17 @@
 package com.testify.ecfeed.ui.dialogs;
 
 import org.eclipse.jface.dialogs.TitleAreaDialog;
-import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.events.SelectionListener;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.FileDialog;
-import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 
 import com.testify.ecfeed.core.resources.ResourceHelper;
 import com.testify.ecfeed.core.serialization.export.TestCasesExportParser;
+import com.testify.ecfeed.ui.common.CompositeFactory;
 import com.testify.ecfeed.ui.dialogs.basic.ExceptionCatchDialog;
 
 public class TestCasesExportDialog extends TitleAreaDialog {
@@ -26,11 +21,13 @@ public class TestCasesExportDialog extends TitleAreaDialog {
 	private TestCasesExportParser fExportParser;
 	private Text fTargetFileText;
 	private String fTargetFile;
+	private CompositeFactory fCompositeFactory;
 
 	public TestCasesExportDialog(Shell parentShell, int methodParametersCount) {
 		super(parentShell);
 		fMethodParametersCount = methodParametersCount;
-		fExportParser = new TestCasesExportParser(); 
+		fExportParser = new TestCasesExportParser();
+		fCompositeFactory = CompositeFactory.getInstance();
 	}
 
 	public static boolean isAdvancedMode() {
@@ -43,7 +40,7 @@ public class TestCasesExportDialog extends TitleAreaDialog {
 		setDialogMessage(this);
 
 		Composite area = (Composite) super.createDialogArea(parent);
-		Composite container = DialogHelper.createGridContainer(area, 1);
+		Composite container = fCompositeFactory.createGridContainer(area, 1);
 
 		if (isAdvancedMode()) {
 			createTemplateDefinitionContainer(container);
@@ -69,15 +66,15 @@ public class TestCasesExportDialog extends TitleAreaDialog {
 	}
 
 	private void createTemplateDefinitionContainer(Composite parent) {
-		Composite container = DialogHelper.createGridContainer(parent, 1);
+		Composite container = fCompositeFactory.createGridContainer(parent, 1);
 		createTemplateComposite(container);
 	}
 
 	private void createTemplateComposite(Composite parent) {
 		final String DEFINE_TEMPLATE = "Define template for export data.";
-		DialogHelper.createLabel(parent, DEFINE_TEMPLATE);		
+		fCompositeFactory.createLabel(parent, DEFINE_TEMPLATE);		
 
-		fTemplateText = DialogHelper.createText(parent, 300, readTemplateFromResource());		
+		fTemplateText = fCompositeFactory.createText(parent, 300, readTemplateFromResource());		
 	}
 
 	private String readTemplateFromResource() {
@@ -95,11 +92,11 @@ public class TestCasesExportDialog extends TitleAreaDialog {
 
 	private void createTargetFileContainer(Composite parent) {
 		final String TARGET_FILE = "Target file";
-		DialogHelper.createLabel(parent, TARGET_FILE);		
+		fCompositeFactory.createLabel(parent, TARGET_FILE);		
 
-		Composite targetFileContainer = DialogHelper.createGridContainer(parent, 2);
-		fTargetFileText = DialogHelper.createFileSelectionText(targetFileContainer);
-		DialogHelper.createBrowseButton(targetFileContainer, new BrowseAdapter()); 
+		Composite targetFileContainer = fCompositeFactory.createGridContainer(parent, 2);
+		fTargetFileText = fCompositeFactory.createFileSelectionText(targetFileContainer);
+		fCompositeFactory.createBrowseButton(targetFileContainer, new BrowseAdapter()); 
 	}
 
 	@Override
@@ -139,59 +136,4 @@ public class TestCasesExportDialog extends TitleAreaDialog {
 			fTargetFileText.setText(dialog.open());
 		}
 	}
-}
-
-class DialogHelper {
-
-	public static Composite createGridContainer(Composite parent, int countOfColumns) {
-
-		Composite container = new Composite(parent, SWT.NONE);
-
-		container.setLayout(new GridLayout(countOfColumns, false));
-		container.setLayoutData(new GridData(GridData.FILL_BOTH));
-
-		return container;
-	}
-
-	public static Text createText(Composite parent, int minimumHeight, String initialText) {
-		Text templateText = new Text(parent, SWT.WRAP|SWT.MULTI|SWT.BORDER|SWT.V_SCROLL);
-
-		GridData gridData = new GridData(SWT.FILL, SWT.FILL, true, true);
-		gridData.minimumHeight = minimumHeight;
-		templateText.setLayoutData(gridData);
-
-		if (initialText != null) {
-			templateText.setText(initialText);
-		}
-
-		return templateText;
-	}
-
-	public static Label createLabel(Composite parent, String text) {
-		Label label = new Label(parent, SWT.NONE);
-		label.setText(text);
-		return label;
-	}
-
-	public static Text createFileSelectionText(Composite targetFileContainer) {
-		Text targetFileText = new Text(targetFileContainer, SWT.BORDER);
-		targetFileText.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false));
-		return targetFileText;
-	}
-
-	public static Button createButton(
-			Composite parent, 
-			String buttonText, 
-			SelectionListener selectionListener) {
-		Button browseButton = new Button(parent, SWT.NONE);
-		browseButton.setText(buttonText);
-		browseButton.addSelectionListener(selectionListener);
-
-		return browseButton;
-	}
-
-	public static Button createBrowseButton(Composite parent, SelectionListener selectionListener) {
-		return createButton(parent, "Browse...", selectionListener);
-	}
-
 }
