41205189482314e1a5fe88d40716e9bc0df93219
Marek Kuczabinski <m.kuczabinski@radytek.com>
2016-07-01 13:24:18 +0200

    Package names changed.

--- ecfeed/test/com/ecfeed/utils/PackageClassHelperTest.java
+++ ecfeed/test/com/ecfeed/utils/PackageClassHelperTest.java
@@ -6,7 +6,7 @@
  * http://www.eclipse.org/legal/epl-v10.html                                     
  ******************************************************************************/
 
-package com.testify.ecfeed.utils;
+package com.ecfeed.utils;
 
 import static org.junit.Assert.assertEquals;
 
--- ecfeed/test/com/ecfeed/utils/StreamHelperTest.java
+++ ecfeed/test/com/ecfeed/utils/StreamHelperTest.java
@@ -6,7 +6,7 @@
  * http://www.eclipse.org/legal/epl-v10.html                                     
  ******************************************************************************/
 
-package com.testify.ecfeed.utils;
+package com.ecfeed.utils;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
