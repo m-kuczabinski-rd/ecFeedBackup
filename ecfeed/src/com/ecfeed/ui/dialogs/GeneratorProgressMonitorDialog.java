6530f8cbd85fdbca4b07d1497f819064b18ae103
Marek Kuczabinski <m.kuczabinski@radytek.com>
2016-07-05 09:58:11 +0200

    Copyrights fixed.

--- ecfeed/src/com/ecfeed/ui/dialogs/GeneratorProgressMonitorDialog.java
+++ ecfeed/src/com/ecfeed/ui/dialogs/GeneratorProgressMonitorDialog.java
@@ -1,13 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2014 Testify AS.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- * 
- * Contributors:
- *     Michal Gluszko (m.gluszko(at)radytek.com) - initial implementation
- ******************************************************************************/
+ *
+ * Copyright (c) 2016 ecFeed AS.                                                
+ * All rights reserved. This program and the accompanying materials              
+ * are made available under the terms of the Eclipse Public License v1.0         
+ * which accompanies this distribution, and is available at                      
+ * http://www.eclipse.org/legal/epl-v10.html 
+ *  
+ *******************************************************************************/
 
 package com.ecfeed.ui.dialogs;
 
--- ecfeed/src/com/ecfeed/ui/dialogs/SetupDialogExecuteOnline.java
+++ ecfeed/src/com/ecfeed/ui/dialogs/SetupDialogExecuteOnline.java
@@ -1,13 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2013 Testify AS.                                                
+ *
+ * Copyright (c) 2016 ecFeed AS.                                                
  * All rights reserved. This program and the accompanying materials              
  * are made available under the terms of the Eclipse Public License v1.0         
  * which accompanies this distribution, and is available at                      
- * http://www.eclipse.org/legal/epl-v10.html                                     
- *                                                                               
- * Contributors:                                                                 
- *     Patryk Chamuczynski (p.chamuczynski(at)radytek.com) - initial implementation
- ******************************************************************************/
+ * http://www.eclipse.org/legal/epl-v10.html 
+ *  
+ *******************************************************************************/
 
 package com.ecfeed.ui.dialogs;
 
