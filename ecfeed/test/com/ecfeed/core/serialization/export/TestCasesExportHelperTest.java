cdec0d23efcb451ad7daa36217af2f652d5145a4
Marek Kuczabinski <m.kuczabinski@radytek.com>
2016-07-05 10:09:44 +0200

    Copyrights fixed.

--- ecfeed/test/com/ecfeed/core/serialization/export/TestCasesExportHelperTest.java
+++ ecfeed/test/com/ecfeed/core/serialization/export/TestCasesExportHelperTest.java
@@ -1,12 +1,14 @@
 package com.ecfeed.core.serialization.export;
 
 /*******************************************************************************
- * Copyright (c) 2016 Testify AS.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- ******************************************************************************/
+ *
+ * Copyright (c) 2016 ecFeed AS.                                                
+ * All rights reserved. This program and the accompanying materials              
+ * are made available under the terms of the Eclipse Public License v1.0         
+ * which accompanies this distribution, and is available at                      
+ * http://www.eclipse.org/legal/epl-v10.html 
+ *  
+ *******************************************************************************/
 
 import static org.junit.Assert.*;
 
